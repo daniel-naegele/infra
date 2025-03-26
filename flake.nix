326b4a88f5851c9f6b4604f870c6b4843f6177c3
Daniel Nägele <daniel@naegele.dev>
2025-03-26 17:36:27 +0100

    add flake setup

---
+++ flake.nix
@@ -0,0 +1,111 @@
+{
+  description = "Flake for own infra monorepo";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (
+      system:
+      with nixpkgs.lib;
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          config.allowUnsupportedSystem = true;
+        };
+        mkNixOsConfiguration =
+          hostname:
+          {
+            system,
+            config,
+            username,
+          }:
+          nameValuePair hostname (nixosSystem {
+            inherit system;
+            modules = [
+              nixos-wsl.nixosModules.wsl
+              (
+                {
+                  inputs,
+                  hostname,
+                  pkgs,
+                  ...
+                }:
+                {
+                  # Set the hostname to the name of the configuration being applied (since the
+                  # configuration being applied is determined by the hostname).
+                  networking.hostName = hostname;
+
+                  # Use the nixpkgs from the flake.
+                  nixpkgs.pkgs = pkgsBySystem.${system};
+
+                  # For compatibility with nix-shell, nix-build, etc.
+                  # See also the setting of NIX_PATH in the home-manager host config
+                  environment.etc.nixpkgs.source = inputs.nixpkgs;
+                  environment.etc.unstable.source = inputs.unstable;
+                  nix.nixPath = [
+                    "nixpkgs=/etc/nixpkgs"
+                    "unstable=/etc/unstable"
+                  ];
+
+                  nix = {
+                    # Don't rely on the configuration to enable a flake-compatible version of Nix.
+                    # package = pkgs.nixFlakes;
+                    extraOptions = "experimental-features = nix-command flakes";
+                    # Re-expose self, nixpkgs and unstable as flakes.
+                    registry = {
+                      self.flake = inputs.self;
+                      nixpkgs = {
+                        from = {
+                          id = "nixpkgs";
+                          type = "indirect";
+                        };
+                        flake = inputs.nixpkgs;
+                      };
+                      unstable = {
+                        from = {
+                          id = "unstable";
+                          type = "indirect";
+                        };
+                        flake = inputs.unstable;
+                      };
+                    };
+                    settings = {
+                      auto-optimise-store = true;
+                    };
+                  };
+                }
+              )
+              (import config)
+              inputs.home-manager.nixosModules.home-manager
+              {
+                home-manager.useUserPackages = true;
+                home-manager.useGlobalPkgs = true;
+                home-manager.backupFileExtension = "backup";
+                home-manager.users.${username} = homeManagerConfigurations."${hostname}";
+                home-manager.extraSpecialArgs = {
+                  inherit hostname inputs;
+                  unstable = unstableBySystem."${system}";
+                };
+              }
+            ];
+            specialArgs = {
+              inherit hostname inputs;
+              unstable = unstableBySystem."${system}";
+            };
+          });
+      in
+      {
+        devShells.default = import ./shell.nix { inherit pkgs; };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
