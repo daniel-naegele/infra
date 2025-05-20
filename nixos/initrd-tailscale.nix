c755adbf525a36fa53213208476cf4fd238dc435
Daniel Nägele <daniel@naegele.dev>
2025-05-20 02:08:12 +0200

    add first sketch of tailscale autostart service

--- nixos/initrd-tailscale.nix
+++ nixos/initrd-tailscale.nix
@@ -34,5 +34,35 @@ in {
       wantedBy = ["initrd.target"];
       serviceConfig.ExecStartPre = "-+/bin/ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf";
       };*/
+
+    # Create a oneshot to autoconnect on rebuild/switch
+    systemd.services.tailscale-autoconnect = {
+      description = "Automatic connection to Tailscale";
+
+      after = [ "network-pre.target" "tailscaled.service" ];
+      wants = [ "network-pre.target" "tailscaled.service" ];
+      wantedBy = [ "initrd.target" ];
+
+      serviceConfig = {
+        Type = "oneshot";
+        Restart= "on-failure";
+      };
+
+      # have the job run this shell script
+      script = with pkgs; ''
+        # wait for tailscaled to settle
+        sleep 5
+
+        # check if we are already authenticated to tailscale
+        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+        if [ $status = "Running" ]; then # if so, then do nothing
+          exit 0
+        fi
+
+        # otherwise authenticate with tailscale using the key from secrets
+        ${tailscale}/bin/tailscale --socket /run/tailscale/tailscaled.sock up --auth-key file:/etc/secrets/ts_auth_key
+      '';
+    };
+
   };
 }
