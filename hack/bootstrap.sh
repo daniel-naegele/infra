759985129c48e208d43f96d0d65e2257b80ed614
Daniel Nägele <daniel@naegele.dev>
2025-09-23 00:27:57 +0200

    sbctl key creation on install

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -20,8 +20,6 @@ ssh-keygen -f "$temp/etc/ssh/ssh_host_ed25519_key" -y | nix run nixpkgs#ssh-to-a
 # Set the correct permissions so sshd will accept the key
 chmod 600 "$temp/etc/ssh/ssh_host_ed25519_key"
 
-# TODO create secure boot keys (hacky wacky)
-
 echo "Please confirm that you updated the SOPS configuration to include this key and updated the keys themselves."
 read -p "Type 'yes' to continue: " confirmation
 if [ "$confirmation" != "yes" ]; then
@@ -34,6 +32,26 @@ read -p "" flake_input
 
 echo "Please define the target host"
 read -p "" target_host
+
+# Set the correct permissions so sshd will accept the key
+chmod 600 "$temp/etc/ssh/ssh_host_ed25519_key"
+# if /var/lib/sbctl already exists
+if [ -d /var/lib/sbctl ]; then
+  sudo mv /var/lib/sbctl /var/lib/sbctl.bak
+fi
+
+sudo sbctl create-keys
+install -d -m755 "$temp/persist/var/lib/sbctl"
+sudo mv /var/lib/sbctl "${temp}/persist/var/lib/sbctl"
+cp -r "${temp}/persist/var" $temp
+
+chmod 600 "$temp/var"
+chmod 600 "$temp/persist/var"
+
+# if you made a backup of sbctl before
+if [ -d /var/lib/sbctl.bak ]; then
+  sudo mv /var/lib/sbctl.bak /var/lib/sbctl
+fi
 
 # Install NixOS to the host system with our secrets
 nix run github:nix-community/nixos-anywhere -- --extra-files "$temp" --flake flake_input --target-host target_host --debug
