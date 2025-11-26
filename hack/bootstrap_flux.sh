e43c21719a35ad19dc95d27d7858c5a7893496e4
Daniel Nägele <daniel@naegele.dev>
2025-11-26 16:56:20 +0100

    make flux use sops decryption

--- hack/bootstrap_flux.sh
+++ hack/bootstrap_flux.sh
@@ -5,6 +5,10 @@ echo "Please provide the cluster path:"
 read -p "" cluster_path
 
 GITHUB_TOKEN=$(sops --decrypt secrets/github-pat.bin)
+DECRYPTION_KEY=$(sops --decrypt secrets/cluster_decryption_key.bin)
+
+sops --decrypt secrets/cluster_decryption_key.bin | kubectl -n flux-system create secret generic sops-age \
+  --from-file=sops.agekey=/dev/stdin
 
 echo "$GITHUB_TOKEN" | flux bootstrap github \
   --token-auth=false \
