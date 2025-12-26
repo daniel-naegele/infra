4db2c15123816f4dc1d38fc421e69d88e85bfd97
Daniel Nägele <daniel@naegele.dev>
2025-12-27 00:31:34 +0100

    fix initrd tailscale

--- nixos/initrd-tailscale.nix
+++ nixos/initrd-tailscale.nix
@@ -18,7 +18,10 @@ in
       "nft_chain_nat"
     ];
 
-    systemd.services.tailscaled = {
+    systemd.services.tailscaled-initrd = {
+      unitConfig = {
+        DefaultDependencies = "no";
+      };
       wantedBy = [ "initrd.target" ];
       before = [
         "cryptsetup-pre.target"
@@ -28,13 +31,14 @@ in
           "PORT=${toString cfg.port}"
           ''"FLAGS=--tun ${lib.escapeShellArg cfg.interfaceName}"''
         ];
+        RuntimeDirectory = "tailscale-initrd";
+        Type = "notify";
+        ExecStart = ''
+          ${pkgs.tailscale}/bin/tailscaled \
+            --state=mem:ts \
+            --socket /run/tailscale-initrd/tailscaled.sock
+        '';
       };
-      script = ''
-        mkdir -p /run/tailscale-initrd
-        ${pkgs.tailscale}/bin/tailscaled \
-          --state=/run/tailscale-initrd.state \
-          --socket /run/tailscale-initrd/tailscaled.sock
-      '';
     };
 
     systemd.network.networks."50-tailscale" = {
@@ -64,17 +68,20 @@ in
     # Create a oneshot to autoconnect on rebuild/switch
     systemd.services.tailscale-autoconnect = {
       description = "Automatic connection to Tailscale";
-
+      unitConfig = {
+        DefaultDependencies = "no";
+      };
       before = [
         "cryptsetup-pre.target"
       ];
       after = [
         "network-pre.target"
-        "tailscaled.service"
+        "tailscaled-initrd.service"
       ];
       wants = [
         "network-pre.target"
-        "tailscaled.service"
+        "tailscaled-initrd.service"
+        "cryptsetup-pre.target"
       ];
       wantedBy = [ "initrd.target" ];
 
@@ -87,12 +94,6 @@ in
       script = with pkgs; ''
         # wait for tailscaled to settle
         sleep 5
-
-        # check if we are already authenticated to tailscale
-        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-        if [ $status = "Running" ]; then # if so, then do nothing
-          exit 0
-        fi
 
         # otherwise authenticate with tailscale using the key from secrets
         ${tailscale}/bin/tailscale \
